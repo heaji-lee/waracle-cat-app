e4d621e655d2f6f6e5f4e18749eb64834ddee4ea
Helen Heaji Lee <helenheajilee@gmail.com>
2025-12-18 23:42:56 +0000

    makes icons on clicked images on small screen & added cat paw cursor

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -104,6 +104,14 @@ export default function Home() {
     const prevCats = catImages;
     try {
       const newFavouriteId = await favouriteCatImage(cat)
+      setSelectedCat((prevCat) =>
+        prevCat && prevCat.id === cat.id
+          ? {
+              ...prevCat,
+              favourite: newFavouriteId ? { id: newFavouriteId } : undefined,
+            }
+          : prevCat
+      )
       setCatImages((prevCats) =>
         prevCats.map((c) => 
           c.id === cat.id ? { ...c, favourite: newFavouriteId ? { id: newFavouriteId } : undefined } : c
@@ -331,7 +339,54 @@ export default function Home() {
             className="fixed inset-0 bg-black/70 flex items-center justify-center z-50"
             onClick={() => setSelectedCat(null)}
           >
-            <div className="relative">
+            <div 
+              className="relative"
+              onClick={(e) => e.stopPropagation()}
+            >
+              <div className="absolute bottom-4 left-1/2 -translate-x-1/2 flex gap-4 sm:hidden z-10">
+                <Button
+                  isIconOnly
+                  variant="ghost"
+                  onClick={() => handleFavourite(selectedCat)}
+                  isDisabled={favouriteLoading === selectedCat.id}
+                  className="text-white rounded-full"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill={selectedCat.favourite?.id ? "red" : "white"}
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke={selectedCat.favourite?.id ? "red" : "white"}
+                    className="size-8"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"
+                    />
+                  </svg>
+                </Button>
+
+                <Button
+                  isIconOnly
+                  variant="ghost"
+                  onClick={() => {
+                    handleDelete(selectedCat)
+                    setSelectedCat(null)
+                  }}
+                  className=" text-white rounded-full"
+                >
+                  <svg 
+                    xmlns="http://www.w3.org/2000/svg" 
+                    fill="none" viewBox="0 0 24 24" 
+                    strokeWidth={1.5} 
+                    stroke="currentColor" 
+                    className="size-8"
+                  >
+                    <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                  </svg>
+                </Button>
+              </div>
               <img
                 src={selectedCat.url}
                 alt={selectedCat.breeds?.[0]?.name ?? "Cat"}
